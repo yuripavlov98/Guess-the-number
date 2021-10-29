57bfce96fefd3ba7e4c6977bb7a0e3d08454b193
Yurion <yuripavlovdex@gmail.com>
2021-10-29 13:59:55 +0300

    step 2

--- Guess the number.py
+++ Guess the number.py
@@ -1,5 +1,16 @@
-import random                    # подключаем модуль
+import random  # подключаем модуль
 
 number = random.randint(1, 101)  # генерируем случайное число от 1 до 100
 
 print('Добро пожаловать в числовую угадайку')
+
+
+def is_valid(num):  # функция проверки введенных данных на корректность
+    if num.isdigit():
+        num = int(num)
+        if 1 <= num <= 100:
+            return True
+        else:
+            return False
+    else:
+        return False
